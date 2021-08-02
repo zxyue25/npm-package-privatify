dbee19ab3e97f2af3629062ee88f0ae7dd2fa5f4
zxyue25 <zxy@syzc.net.cn>
2021-08-02 17:45:43 +0800

    feat: jest

---
+++ src/__tests__/create.spec.ts
@@ -0,0 +1,9 @@
+// import create from '../commands/create'
+
+// describe('创建私服仓库', () => {
+//   test('创建私服仓库', async () => {
+//     await create.action("projectName")
+//   })
+// })
+
+console.log("ddd")
--- src/__tests__/create.test.ts
+++
@@ -1,7 +0,0 @@
-import create from '../commands/create'
-
-describe('创建私服仓库', () => {
-  test('创建私服仓库', async () => {
-    await create.action("projectName")
-  })
-})
