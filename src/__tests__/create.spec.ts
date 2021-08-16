bfa23dd62a5fa6d1fccc61877fc3194311fe2550
zxyue25 <zxy@syzc.net.cn>
2021-08-16 11:37:34 +0800

    test:create

--- src/__tests__/create.spec.ts
+++ src/__tests__/create.spec.ts
@@ -1,8 +1,26 @@
-import createAction from '../commands/create'
+import { cloneProject } from '../commands/create'
+import * as path from 'path'
+import { fs } from '../lib'
+import { readFile } from '../commands/utils'
 
+const targetDir = path.join(__dirname, './_test-pro1')
 describe('privatify create', () => {
   test('创建私服仓库', async () => {
     const projectName = '_test-pro1'
-    await createAction.action(projectName)
+    const projectInfo = {
+      name: '_test-pro1',
+      description: 'test privatify create cloneProject',
+      author: 'zxyue25',
+    }
+    await cloneProject(targetDir, projectName, projectInfo)
+    expect(fs.ensureDir(targetDir)).toBeTruthy()
+    const packageJson = readFile('src/__tests__/_test-pro1')
+    expect(packageJson.name).toBe(projectInfo.name)
+    expect(packageJson.description).toBe(projectInfo.description)
+    expect(packageJson.author).toBe(projectInfo.author)
+  })
+
+  afterAll(() => {
+    fs.remove(targetDir)
   })
 })
--- src/commands/create.ts
+++ src/commands/create.ts
@@ -13,9 +13,8 @@ import {
   info,
 } from '../lib'
 
-
 // 检查是否已经存在相同名字工程
-const checkProjectExist = async (targetDir) => {
+export const checkProjectExist = async (targetDir) => {
   if (fs.existsSync(targetDir)) {
     const answer = await inquirer.prompt({
       type: 'list',
@@ -33,54 +32,63 @@ const checkProjectExist = async (targetDir) => {
   return false
 }
 
+export const getQuestions = async (projectName) => {
+  return await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: `package name: (${projectName})`,
+      default: projectName,
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'description',
+    },
+    {
+      type: 'input',
+      name: 'author',
+      message: 'author',
+    },
+  ])
+}
+
+export const cloneProject = async (targetDir, projectName, projectInfo) => {
+  startSpinner(`开始创建私服仓库 ${chalk.cyan(targetDir)}`)
+  // 复制'private-server-boilerplate'到目标路径下创建工程
+  await fs.copy(
+    path.join(__dirname, '..', '..', 'private-server-boilerplate'),
+    targetDir
+  )
+
+  // handlebars模版引擎解析用户输入的信息存在package.json
+  const jsonPath = `${targetDir}/package.json`
+  const jsonContent = fs.readFileSync(jsonPath, 'utf-8')
+  const jsonResult = handlebars.compile(jsonContent)(projectInfo)
+  fs.writeFileSync(jsonPath, jsonResult)
+
+  // 新建工程装包
+  execa.commandSync('npm install', {
+    stdio: 'inherit',
+    cwd: targetDir,
+  })
+
+  succeedSpiner(
+    `私服仓库创建完成 ${chalk.yellow(projectName)}\n👉 输入以下命令开启私服:`
+  )
+
+  info(`$ cd ${projectName}\n$ sh start.sh\n`)
+}
+
 const action = async (projectName: string, cmdArgs?: any) => {
   try {
-    const targetDir = path.join(cmdArgs && cmdArgs.context || cwd, projectName)
+    const targetDir = path.join(
+      (cmdArgs && cmdArgs.context) || cwd,
+      projectName
+    )
     if (!(await checkProjectExist(targetDir))) {
-      startSpinner(`开始创建私服仓库 ${chalk.cyan(targetDir)}`)
-      // 复制'private-server-boilerplate'到目标路径下创建工程
-      await fs.copy(
-        path.join(__dirname, '..', '..', 'private-server-boilerplate'),
-        targetDir
-      )
-      const answers = await inquirer.prompt([
-        {
-          type: 'input',
-          name: 'name',
-          message: `package name: (${projectName})`,
-          default: projectName,
-        },
-        {
-          type: 'input',
-          name: 'description',
-          message: 'description',
-        },
-        {
-          type: 'input',
-          name: 'author',
-          message: 'author',
-        },
-      ])
-
-      // handlebars模版引擎解析用户输入的信息存在package.json
-      const jsonPath = `${targetDir}/package.json`
-      const jsonContent = fs.readFileSync(jsonPath, 'utf-8')
-      const jsonResult = handlebars.compile(jsonContent)(answers)
-      fs.writeFileSync(jsonPath, jsonResult)
-
-      // 新建工程装包
-      execa.commandSync('npm install', {
-        stdio: 'inherit',
-        cwd: targetDir,
-      })
-
-      succeedSpiner(
-        `私服仓库创建完成 ${chalk.yellow(
-          projectName
-        )}\n👉 输入以下命令开启私服:`
-      )
-
-      info(`$ cd ${projectName}\n$ sh start.sh\n`)
+      const projectInfo = await getQuestions(projectName)
+      await cloneProject(targetDir, projectName, projectInfo)
     }
   } catch (err) {
     failSpinner(err)
