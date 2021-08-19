ed34749edd449d0e6fefdd7cf850c8409066f4c5
zxyue25 <zxy@syzc.net.cn>
2021-08-19 10:55:08 +0800

    fix: build

--- src/__tests__/check-package.spec.ts
+++ src/__tests__/check-package.spec.ts
@@ -7,5 +7,5 @@ test('检查package.json下不存在scope私包', () => {
 })
 
 test('检查package.json下存在scope私包', () => {
-  expect(checkPackage(packageJson, '@jdd/**')).toEqual(["@jdd/cli-plugin-babel", "@jdd/cli-plugin-vue", "@jdd/cli-service"])
+  expect(checkPackage(packageJson, '@jdd/**')).toEqual(['@jdd/cli-plugin-babel', '@jdd/cli-plugin-vue', '@jdd/cli-service'])
 })
